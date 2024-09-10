f4e8d49d2e1c4955efd7e7c7e5a933a427afe1bf
Don <496156+nikolat@users.noreply.github.com>
2024-09-10 19:00:08 +0900

    fix

--- src/response.ts
+++ src/response.ts
@@ -64,9 +64,9 @@ const isAllowedToPost = (event: NostrEvent) => {
 
 const getResmap = (): [RegExp, (event: NostrEvent, regstr: RegExp) => Promise<[string, string[][]]> | [string, string[][]]][] => {
 	const resmapReply: [RegExp, (event: NostrEvent, regstr: RegExp) => Promise<[string, string[][]]> | [string, string[][]]][] = [
-		[/shanten\s(([<>()0-9mpsz]){,44})$/, res_shanten],
-		[/score\s(([<>()0-9mpsz]){,42})\s([0-9][mpsz])(\s([0-9][mpsz]))?(\s([0-9][mpsz]))?$/, res_score],
-		[/machi\s(([<>()0-9mpsz]){,42})$/, res_machi],
+		[/shanten\s(([<>()0-9mpsz]){2,44})$/, res_shanten],
+		[/score\s(([<>()0-9mpsz]){2,42})\s([0-9][mpsz])(\s([0-9][mpsz]))?(\s([0-9][mpsz]))?$/, res_score],
+		[/machi\s(([<>()0-9mpsz]){2,42})$/, res_machi],
 	];
 	return resmapReply;
 };
